265e8d3c6091bd0ef3a2264afe39c8b8daf9519a
AidenArgents <xuyan1993714@gmail.com>
2018-01-26 17:27:45 +0800

    Update muban.js

--- muban.js
+++ muban.js
@@ -4,19 +4,19 @@ var json = {
 			title:"取消长时间未发货EUB",value:"Dear friend, postoffice said the item you buy is contraband, won't send to your address, so not send yet, and ebay will inspected about send parcel handle time, and in official transport in ePack or not, could you kindly cancel this order for us, cause you a load of problem, so sorry. I know it sounds ridiculous, but contraband to office could be anything, I recently got a wallpaper return because it's a worldwide map pattern wallpaper."
 		},
 	        {
-			title:'其他国家10-30天来未收到',value:'Not that fast from China to your country, please wait patient, normally it will take 3-5 weeks for the item to get into your local post,would you mind to give it more time ?  Is that acceptable? Please don't worry that, we are here for you if you have other problems. '
+			title:'其他国家10-30天来未收到',value:`Not that fast from China to your country, please wait patient, normally it will take 3-5 weeks for the item to get into your local post,would you mind to give it more time ?  Is that acceptable? Please don't worry that, we are here for you if you have other problems. `
 		},
                 {
-			title:'超过35 days',value:'We Checked Transaction Information, Your Package Is Already In Your Country, Please Check, Tracking Number Is 80499940308. Would You Mind Wait More Time? Maybe you can go post office to check.'
+			title:'超过35 days',value:`We Checked Transaction Information, Your Package Is Already In Your Country, Please Check, Tracking Number Is 80499940308. Would You Mind Wait More Time? Maybe you can go post office to check.`
 		},
                 {
-			title:'退包',value:'It seems like your parcel returned by your local post office, do not worry, we will refund you after we received the package, you can check by yourself, the tracking number is '
+			title:'退包',value:`It seems like your parcel returned by your local post office, do not worry, we will refund you after we received the package, you can check by yourself, the tracking number is `
 		},
                 {
-			title:'退款',value:'Refund you now. If did not receive in 24 hours, please contact us again, lately have some problem with our finance department.'
+			title:'退款',value:`Refund you now. If did not receive in 24 hours, please contact us again, lately have some problem with our finance department.`
 		},
                 {
-			title:'买家收到物品 并且很满意，让买家确认物品已准时送达',value:'Dear friend,I’m so glad to hear that. Thank you very much for your kind message, we are happy to know that you have received your item. If you like our item or if you are satisfied with our service, hope you can leave us a positive feedback with all five star, meanwhile,you can confirm the item was delivered on time(connect to on-time delivery question, please say YES), that is very important to our business success. Thank you. Looking forward to next cooperation. Best wishes.'
+			title:'买家收到物品 并且很满意，让买家确认物品已准时送达',value:`Dear friend,I’m so glad to hear that. Thank you very much for your kind message, we are happy to know that you have received your item. If you like our item or if you are satisfied with our service, hope you can leave us a positive feedback with all five star, meanwhile,you can confirm the item was delivered on time(connect to on-time delivery question, please say YES), that is very important to our business success. Thank you. Looking forward to next cooperation. Best wishes.`
 		}
 	     ]
            };
