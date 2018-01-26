75a249133ebffafa48dc42bcd8307a6445928a48
AidenArgents <xuyan1993714@gmail.com>
2018-01-26 16:12:52 +0800

    Create muban.js

---
+++ muban.js
@@ -0,0 +1,7 @@
+var json = {
+	data:[
+		{
+			title:"取消长时间未发货EUB",value:"Dear friend, postoffice said the item you buy is contraband, won't send to your address, so not send yet, and ebay will inspected about send parcel handle time, and in official transport in ePack or not, could you kindly cancel this order for us, cause you a load of problem, so sorry. I know it sounds ridiculous, but contraband to office could be anything, I recently got a wallpaper return because it's a worldwide map pattern wallpaper."
+		}
+	     ]
+           };
