ed95a1f059ad88c2fe1d1b46d985a72dc986e37a
AidenArgents <xuyan1993714@gmail.com>
2018-04-06 17:26:47 +0800

    Update muban.js

--- muban.js
+++ muban.js
@@ -4,19 +4,19 @@ var json = {
 			title:"取消长时间未发货EUB",value:"Dear friend, postoffice said the item you buy is contraband, won't send to your address, so not send yet, and ebay will inspected about send parcel handle time, and in official transport in ePack or not, could you kindly cancel this order for us, cause you a load of problem, so sorry. I know it sounds ridiculous, but contraband to office could be anything, I recently got a wallpaper return because it's a worldwide map pattern wallpaper."
 		},
 	        {
-			title:'其他国家10-30天来未收到',value:`Not that fast from China to your country, please wait patient, normally it will take 3-5 weeks for the item to get into your local post,would you mind to give it more time ?  Is that acceptable? Please don't worry that, we are here for you if you have other problems. 
+			title:'其他国家10-30天来未收到',value:`Not that fast from China to your country, please wait patient, normally it will take 3-5 weeks for the item to get into your local post, would you mind to give it more time? Is that acceptable? Please don't worry that, we are here for you if you have other problems.
 `},
                 {
-			title:'超过35 days',value:`We Checked Transaction Information, Your Package Is Already In Your Country, Please Check, Tracking Number Is 80499940308. Would You Mind Wait More Time? Maybe you can go post office to check.
+			title:'超过35 days',value:`We checked transaction information, your package is already in your country, please check, tracking number is already upload on eBay. Normally it will take 3-5 weeks for the item to get into your local post office, it could delayed a little more on transport, really normal, would you mind wait more time? Maybe you can go post office to check. If it not received in 60 days in the end (which a few people's delayed time), over that, could be lost, will refund you ASAP, hope you could understand, thanks very much. Have a nice day!
 `},
                 {
-			title:'退包',value:`It seems like your parcel returned by your local post office, do not worry, we will refund you after we received the package, you can check by yourself, the tracking number is 
+			title:'退包',value:`It seems like your parcel returned by your local post office, do not worry, we will refund you after we received the package, you can also check by yourself, hope you are not leave bad review so earily, the tracking number is 
 `},
                 {
 			title:'退款',value:`Refund you now. If did not receive in 24 hours, please contact us again, lately have some problem with our finance department. Hope you can leave a positive feedback, sorry about this situation.
 `},
 		{
-			title:'图片',value:`Please send picture for us to check, and picture of shipping label on package, a codebar tip on product wrapper would be even better. Thank you.
+			title:'图片',value:`Please send product picture for us to check, and picture of shipping label on package, a codebar tip on product wrapper would be even better. Thank you.
 `},
 		{
 			title:'作废订单来找',value:`Sorry for this situation, I can refund you first, since it could lost after this long time, could you leave me a positive feedback, then wait for this product?
@@ -31,16 +31,19 @@ var json = {
 			title:'少发',value:`I'm sorry about this, it must be forgotten when packing, our warehouse worker have lots of package need to prepare everyday, hope you will belive and understand, would you kindly accept refund of this missing product? Could you please not leaving bad review?
 `},
 		{
+			title:'错发',value:`I'm sorry about this, it must be wrong-packed when packing, our warehouse worker have lots of package need to prepare everyday, hope you will belive and understand, would you kindly accept refund of this wrong sent product? Could you please not leaving bad review?
+`},
+		{
 			title:'回给冷静的买家',value:`Thank you for understanding, hope you can get your item soon! have a nice day.
 `},
 		{
 			title:'同意假重发',value:`Thank you for understanding, we will arrange it in 12 hours, have a nice day.
 `},
 		{
-			title:'回好评',value:`thanks for your long time support. have a nice day.
+			title:'一般的回复',value:`thanks for your long time support. have a nice day.
 `},
-                {
-			title:'买家收到物品 并且很满意，让买家确认物品已准时送达',value:`Dear friend,I’m so glad to hear that. Thank you very much for your kind message, we are happy to know that you have received your item. If you like our item or if you are satisfied with our service, hope you can leave us a positive feedback with all five star, meanwhile,you can confirm the item was delivered on time(connect to on-time delivery question, please say YES), that is very important to our business success. Thank you. Looking forward to next cooperation. Best wishes.
+		{
+			title:'买家收到物品 并且很满意，让买家确认物品已准时送达',value:`Dear friend,I’m so glad to hear that. Thank you very much for your kind message, we are happy to know that you have received your item. If you like our item or if you are satisfied with our service, hope you can leave us a positive feedback with all five star, meanwhile, you can confirm the item was delivered on time(connect to on-time delivery question, please say YES), that is very important to our business success. Thank you. Looking forward to next cooperation. Best wishes.
 `}
 	     ]
            };
